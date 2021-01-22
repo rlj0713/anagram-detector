d0bbf0714980cad6e6ba0f7f3dbd8680abb5bc7b
Reid <rlj0713@gmail.com>
2021-01-21 19:31:58 -0500

    commit tree e830c9952ee52c0f76ef9a178c27b288127ac8df

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :input
+
+    def initialize(input)
+        @input = input
+    end
+    
+    def match(argument)
+      array = []
+        argument.each do |item|
+        if input.split("").sort == item.split("").sort
+            array << item
+        end
+      end
+      array
+    end
+end
